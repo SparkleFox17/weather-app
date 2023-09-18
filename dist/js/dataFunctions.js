64029f4d92b56b4d207222713e986d431bf0b86b
Stephanie Tait <stephanie133.tait@gmail.com>
2023-09-18 14:21:04 -0400

    added serverless functions

--- dist/js/dataFunctions.js
+++ dist/js/dataFunctions.js
@@ -1,4 +1,4 @@
-//const WEATHER_API_KEY = "F59a3ec92c88e62dd98edb38281c193d"; //remove breaks app 
+//const WEATHER_API_KEY = "F59a3ec92c88e62dd98edb38281c193d"; //remove breaks app 4:36:21 // api key
 
 export const setLocationObject = (locationObj, coordsObj) => {
     const { lat, lon, name, unit } = coordsObj;
@@ -14,15 +14,31 @@ export const getHomeLocation = () => {
     return localStorage.getItem(defaultWeatherLocation);
 };
 
-export const getWeatherFromCoords = async( locationObj) => {
-    const lat = locationObj.getlat();
-    const lon = locationObj.getlon();
-    const units = locationObj.getUnit();
-    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&
-    exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
+export const getWeatherFromCoords = async(locationObj) => {
+    //const lat = locationObj.getlat();
+    //const lon = locationObj.getlon();
+    //const units = locationObj.getUnit();
+    //const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&
+    //exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
+    //try {
+        //const weatherStream = awaitfetch(url);
+        //const weatherJson = await weatherStream.json();
+        //return weatherJson;
+    //} catch (err) {
+        //console.error(err);
+    //}
+
+    const urlDataObj = {
+         lat: locationObj.getlat(),
+         lon: locationObj.getlon(),
+         units: locationObj.getUnit()
+    };
     try {
-        const weather = awaitfetch(url);
-        const weatherJson = await weatherstream.json();
+        const weatherStream = awaitfetch("./.netlify/functions/get_weather", {
+            method: "POST",
+            body: JSON.stringify(urlDataObj)
+        });
+        const weatherJson = await weatherStream.json();
         return weatherJson;
     } catch (err) {
         console.error(err);
@@ -44,6 +60,10 @@ export const getChoordsFromApi = async (entryText, units) => {
     }
 };  
  
+const urlDataObj = {
+    text: entryText,
+    units: units
+}
 
 export const cleanText = (text) => {
     const regex = / {2,}/g;
